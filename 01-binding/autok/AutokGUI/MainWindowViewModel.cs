5f705905ea62942e9096c1702c886ccf1b4c4144
Szomor Levente Péter <59541573+SzmrLevee@users.noreply.github.com>
2025-10-19 15:20:13 +0200

    Add files via upload

---
+++ 01-binding/autok/AutokGUI/MainWindowViewModel.cs
@@ -0,0 +1,71 @@
+﻿using AutokLib;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AutokGUI
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public MainWindowViewModel() => DataStore.Initialize();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public static IEnumerable<string>? Makes => DataStore
+            .Instance?
+            .Cars
+            .Select(x => x.Make)
+            .Distinct();
+
+        public IEnumerable<string>? AvailableModels => DataStore
+            .Instance?
+            .Cars
+            .Where(x => x.Make == SelectedMake)
+            .Select(x => x.Model);
+
+        private string? selectedMake = null;
+        private string? selectedModel = null;
+
+        public bool IsMakeSelected => selectedMake is not null;
+
+        public string? SelectedMake
+        {
+            get => selectedMake;
+            set
+            {
+                selectedMake = value;
+
+                Changed(nameof(IsMakeSelected));
+                Changed(nameof(AvailableModels));
+            }
+        }
+
+        public string? SelectedModel
+        {
+            get => selectedModel;
+            set
+            {
+                selectedModel = value;
+
+                Changed(nameof(GeneratedLicencePlate));
+            }
+        }
+
+        public string GeneratedLicencePlate
+        {
+            get
+            {
+                var car = DataStore
+                    .Instance?
+                    .Cars?
+                    .FirstOrDefault(x => x.Model == SelectedModel);
+
+                return car is null
+                    ? string.Empty
+                    : $"{car.Make[0]}{car.Model[0]}{car.Fuel[0]}-{Random.Shared.Next(100, 999)}";
+            }
+        }
+
+        private void Changed([CallerMemberName] string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
